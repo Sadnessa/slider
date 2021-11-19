60cd80d129f83f5dd7d0dc14362bbc02040b6060
Darya Volkova <anrich@bk.ru>
2021-11-19 18:10:51 +0200

    fixed card background

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,8 +11,6 @@
       display: flex;
      justify-content: center; 
      align-items: center;
-    background-color: rgb(180, 179, 179);
-    box-shadow: -1px -1px 15px 2px rgba(34, 60, 80, 0.2);
 }
 
 </style>
